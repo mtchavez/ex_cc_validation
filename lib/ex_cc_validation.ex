1752c25f42317e4cb7b4d5a3a9bc3549f6ad7986
Chavez <matthew@el-chavez.me>
2017-06-21 16:49:08 -0700

    Project setup
    - Run mix new
    - Add .travis.yml

---
+++ lib/ex_cc_validation.ex
@@ -0,0 +1,18 @@
+defmodule CcValidation do
+  @moduledoc """
+  Documentation for CcValidation.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CcValidation.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_cc_validation_test.exs
@@ -0,0 +1,8 @@
+defmodule CcValidationTest do
+  use ExUnit.Case
+  doctest CcValidation
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
