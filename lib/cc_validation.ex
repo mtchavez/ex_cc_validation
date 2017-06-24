ab8f6198ecb9b1b143ab661c2727a92a41e4f8d6
Chavez <matthew@el-chavez.me>
2017-06-23 21:00:44 -0700

    Implement Luhn algorithm check

--- lib/cc_validation.ex
+++ lib/cc_validation.ex
@@ -22,4 +22,31 @@ defmodule CcValidation do
     when byte_size(number) < 13 or byte_size(number) > 19 do
       {:error, false}
   end
+
+  @doc """
+  Validate a credit card number using Luhn Algorithm
+
+  ## Examples
+
+  Valid credit card
+      iex> CcValidation.validate("4716892095589823")
+      {:ok, true}
+
+  Invalid credit card
+      iex> CcValidation.validate("4024007106963134")
+      {:error, false}
+  """
+  def validate(number) do
+    total = String.codepoints(number)
+    |> Enum.reverse
+    |> Stream.with_index
+    |> Enum.filter_map(fn({_, i}) -> rem(i + 1, 2) == 0 end, fn({n, _}) -> String.to_integer(n) * 2 end)
+    |> Enum.sum
+    case rem(total, 10) do
+      0 ->
+        {:ok, true}
+      _ ->
+        {:error, false}
+    end
+  end
 end
