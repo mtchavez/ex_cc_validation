3943a3d0971f1fe96e3bf214193e25937018979b
Chavez <matthew@el-chavez.me>
2017-06-24 18:43:48 -0700

    Validate a test card without check test cards true
    - Return false unless checking test cards is true for test cards

--- lib/cc_validation.ex
+++ lib/cc_validation.ex
@@ -3,10 +3,24 @@ defmodule CcValidation do
   CcValidation implements methods to verify validity of a credit card number
   """
 
-  def validate(number, check_test_numbers \\ false)
-
   @doc """
-  Validate test card numbers when check_test_numbers is true
+  Validate a credit card number using Luhn Algorithm
+
+  ## Examples
+
+  Valid credit card
+      iex> CcValidation.validate("4716892095589823")
+      {:ok, true}
+
+  Invalid credit card
+      iex> CcValidation.validate("4024007106963124")
+      {:error, false}
+
+  Test card returns false
+      iex> CcValidation.validate("4111111111111111")
+      {:error, false}
+
+  ### Validate test card numbers when check_test_numbers is true
 
   A valid test card
       iex> CcValidation.validate("4111111111111111", true)
@@ -21,7 +35,16 @@ defmodule CcValidation do
   a test card
       iex> CcValidation.validate("4716892095589823", true)
       {:ok, true, test_number: false}
+
+  ### Card numbers that are of length less than 13 or greater than 19 are invalid.
+
+      iex> CcValidation.validate("123")
+      {:error, false}
+
+      iex> CcValidation.validate("12345678901234567890")
+      {:error, false}
   """
+  def validate(number, check_test_numbers \\ false)
   def validate(number, true) do
     case CcValidation.TestNumbers.has_number?(number) do
       true ->
@@ -30,60 +53,39 @@ defmodule CcValidation do
         Tuple.append(validate(number), test_number: false)
     end
   end
-
-  @doc """
-  Card numbers that are of length less than 13 or greater than 19 are invalid.
-
-  ## Examples
-
-      iex> CcValidation.validate("123")
-      {:error, false}
-
-      iex> CcValidation.validate("12345678901234567890")
-      {:error, false}
-  """
   def validate(number, false)
     when byte_size(number) < 13 or byte_size(number) > 19 do
       {:error, false}
   end
-
-  @doc """
-  Validate a credit card number using Luhn Algorithm
-
-  ## Examples
-
-  Valid credit card
-      iex> CcValidation.validate("4716892095589823")
-      {:ok, true}
-
-  Invalid credit card
-      iex> CcValidation.validate("4024007106963124")
-      {:error, false}
-  """
   def validate(number, false) do
-    total = String.codepoints(number)
-    |> Enum.reverse
-    |> Stream.with_index
-    |> Enum.map(fn {num, i} ->
-        remainder =  rem(i + 1, 2)
-        if remainder == 0 do
-          doubled = String.to_integer(num) * 2
-          if doubled > 9 do
-            doubled - 9
-          else
-            doubled
-          end
-        else
-          String.to_integer(num)
+    case CcValidation.TestNumbers.has_number?(number) do
+      true ->
+        {:error, false}
+      _ ->
+      total = String.codepoints(number)
+      |> Enum.reverse
+      |> Stream.with_index
+      |> Enum.map(fn {num, i} ->
+        case rem(i + 1, 2) do
+          0 ->
+            doubled = String.to_integer(num) * 2
+            if doubled > 9 do
+              doubled - 9
+            else
+              doubled
+            end
+          _ ->
+            String.to_integer(num)
         end
       end)
-    |> Enum.sum
+      |> Enum.sum
 
-    case rem(total, 10) do
-      0 ->
-        {:ok, true}
-      _ ->
-        {:error, false}
+      case rem(total, 10) do
+        0 ->
+          {:ok, true}
+        _ ->
+          {:error, false}
+      end
     end
   end
 end
