e7461abf8d970e21e78abe664c6bfee55b509408
Chavez <matthew@el-chavez.me>
2017-07-13 12:42:32 -0700

    Update version to 0.1.1
    - Update CHANGELOG
    - Update mix package version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CcValidation.Mixfile do
   def project do
     [
       app: :cc_validation,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
